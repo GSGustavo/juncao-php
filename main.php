3a71469996e7be521f0ef8413923dd04a5041bd7
Gustavo Gonzaga da Silva <47232603+GSGustavo@users.noreply.github.com>
2023-11-11 18:11:47 -0400

    Create main.php

---
+++ main.php
@@ -0,0 +1,23 @@
+$mensagem = "";
+
+$nomes = ["Maça", 'Uva', "Laranja"];
+
+$juncoes = [];
+if (count($nomes) > 0) {
+    if (count($nomes) == 2) {
+        $juncoes[] = " e ";
+    } else {
+        for ($i = 1; $i < count($nomes) - 1; $i++) {
+            $juncoes[] = ", ";
+        }
+        $juncoes[] = " e ";
+    }
+
+    $count = 0;
+    foreach ($nomes as $farmacia) {
+        $mensagem .= $farmacia . ($count + 1 == count($nomes) ? "" : $juncoes[$count]);
+        $count++;
+    }
+}
+
+echo $mensagem;
